dc12a1f97231424a111ac373ffb363f56ec1d648
DreamingRaven <geo.george@live.co.uk>
2018-01-14 02:47:49 +0000

    restructured and wokin

--- VulkanEngine.cpp
+++ VulkanEngine.cpp
@@ -9,13 +9,9 @@
 void VulkanEngine::initWindow() {
 
     glfwInit();
-
+    // configure glfw through hints
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-    width_m = 800;
-    height_m = 600;
-    std::cout << "Window size (h|w)= " << width_m << " | " << height_m << std::endl;
-
     window_m = glfwCreateWindow(width_m, height_m, WINDOW_NAME, nullptr, nullptr);
 }
 
@@ -23,9 +19,7 @@ void VulkanEngine::initVulkan() {
 
     uint32_t extensionCount = 0;
     vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
     std::cout << extensionCount << " extensions supported" << std::endl;
-
     glm::mat4 matrix;
     glm::vec4 vec;
     auto test = matrix * vec;
@@ -37,17 +31,20 @@ void VulkanEngine::mainLoop() {
         glfwPollEvents();
     }
 
-    glfwDestroyWindow(window_m);
 }
 
 void VulkanEngine::cleanup() {
 
+    glfwDestroyWindow(window_m);
+    glfwTerminate();
 }
 
 // public: ---
 
 void VulkanEngine::run() {
 
+    width_m  = 800; // TODO implement constructor that assighns these
+    height_m = 600;
     glfwTerminate();
     initWindow();
     initVulkan();
--- src/ExampleVariableArrayTemplate.tpp
+++
@@ -1,6 +0,0 @@
-// C++ Hack to seperate templates from header files, and not in cpp files by convention as not true compilation
-
-template<typename T,int N>
-int ExampleVariableArrayTemplate<T, N>::getSize() {
-    return N;
-}
